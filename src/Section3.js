ee52a2a912f4832ad615a776072a711d40a0c813
hwanza <104492070+hwanza@users.noreply.github.com>
2024-06-10 21:24:06 +0900

    Section3 초안

--- src/Section3.js
+++ src/Section3.js
@@ -3,14 +3,18 @@ import './Section3.css';
 
 const Section3 = () => {
     return (
-        <div className="Section3">
+        <div className="Section3 fade-in-up_3">
             <div className="Section3-Top_SubTitle">
                 <div className="Section3-Text">
-                <span>부모와 자녀가 함께 성장하는 공간</span><br></br>
-                <span>일기장으로 가족의 일상을 공유하고, 더 가까워지는 시간을 가져보세요</span>
+                    <div className="Section3-MainText">
+                        <span>부모와 자녀가 함께 성장하는 <strong>공간</strong></span><br></br>
+                    </div>
+                    <div className="Section3-SubText">
+                        <span>일기장으로 가족의 일상을 공유하고, 더 가까워지는 시간을 가져보세요</span>
+                    </div>
                 </div>
             </div>
-            <div className="Section3_Img">
+            <div className="Section3-Img">
                 <img src={process.env.PUBLIC_URL + '/Diary_2.png'} alt="App Logo" />
             </div>
         </div>
